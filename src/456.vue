1cf80117ff14b33f775f753d07dd4939c71cd5bb
zhangiszhang <1154041905@qq.com>
2018-05-03 14:40:26 +0800

    456

---
+++ src/456.vue
@@ -0,0 +1,29 @@
+<style lang='less' scoped>
+</style>
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  created() {},
+
+  updated() {},
+
+  mounted() {},
+
+  destroyed() {},
+
+  methods: {}
+}
+
+</script>
